89414ab3ad66f52734817f3ab0bea5b3b07ba3cf
irisjennese <irisjennese.raymundo@gmail.com>
2023-05-12 13:18:59 +0800

    added redux persist on cart

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartSlice from './slices/cartSlice';
+import { persistReducer, persistStore } from"redux-persist";
+import storage from 'redux-persist/lib/storage';
 
-const store = configureStore({
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+
+
+
+const persisterReducer = persistReducer(persistConfig, cartSlice);
+
+export const store = configureStore({
   reducer: {
-    cart: cartSlice,
+    cart: persisterReducer,
   },
 });
 
+
+
+
 export default store;
+
+export const persistor = persistStore(store);
